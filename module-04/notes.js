2f10a6cda0ddff26f69dfb116760903d664b2045
Matthew Selby <>
2021-11-26 14:17:58 -0700

    working on module 04

---
+++ module-04/notes.js
@@ -0,0 +1,17 @@
+// the DOM
+// The window object = everthing in the window
+// window.location
+// the window sets Global Variables
+
+// The Document
+// document
+// opening HTML to closting HTML tag
+
+// navigator
+//
+
+// create HTML
+// console.log(`<<<<<     Create HTML     >>>>>`);
+//
+// const myParagraph = document.createElement('p');
+// myParagraph.textContent = `I am a super cool p created with javascript!`;
