7b7135811fe730f81f1cbcfbbf6bfd2f06412b39
Matthew Selby <matthewselby@users.noreply.github.com>
2023-02-01 22:39:46 -0700

    working with strings

---
+++ playground/types.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+const name = 'matthew';
+const middle = "clark";
+const last = `selby `;
+
+const sentance = `He's so coooool.`;
+const sentanceTwo = 'He\'s also smart.';
+
+const looooong = `ohhhhh
+
+yeah
+
+I
+
+like
+
+Pizza
+`;
+
+// Concatination - a series of interconnected things or events. OR LINKING THINGS TOGETHER
+// Interpolation - the insertion of something of a different nature into something else.
+
+const hello = `Hello, my name is ${name}. It's nice to meet you. I am ${1+100} years old.`;
