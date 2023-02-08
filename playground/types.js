cbe7d09aef084d7f2f2f665d974314cead01e624
Matthew Selby <matthewselby@users.noreply.github.com>
2023-02-07 22:49:06 -0700

    going through numbers

--- playground/types.js
+++ playground/types.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-const name = 'matthew';
+// Strings
+const first = 'matthew';
 const middle = "clark";
 const last = `selby `;
 
@@ -20,4 +21,19 @@ Pizza
 // Concatination - a series of interconnected things or events. OR LINKING THINGS TOGETHER
 // Interpolation - the insertion of something of a different nature into something else.
 
-const hello = `Hello, my name is ${name}. It's nice to meet you. I am ${1+100} years old.`;
+const hello = `Hello, my name is ${first}. It's nice to meet you. I am ${1+100} years old.`;
+
+// Numbers
+const coolInteger = 100;
+const coolFloat = 100.01;
+const stringsAreLoaded = "100" + "1";
+const round = math.round(100.9);
+
+const candy = 80;
+const kids = 3;
+const kidsGet = candy / kids;
+console.log(`Each kid gets ${kidsGet} pieces of candy.`);
+
+const floorKidsGet = math.floor( candy / kids );
+const dadGets = candy % kids;
+console.log(`But really, each kid gets ${floorKidsGet} and dad gets ${dadGets} pieces of candy.`);
